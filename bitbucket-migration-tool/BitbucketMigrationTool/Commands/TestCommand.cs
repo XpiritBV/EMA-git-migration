d5f3e9a506889f4e270c4a06d4f47a77c9762755
Sujith Quintelier <sujith.quintelier@gmail.com>
2023-09-08 10:08:55 +0200

    Add test command
    Azure DevOps
    Bitbucket

---
+++ bitbucket-migration-tool/BitbucketMigrationTool/Commands/TestCommand.cs
@@ -0,0 +1,35 @@
+﻿using BitbucketMigrationTool.Models;
+using BitbucketMigrationTool.Services;
+using McMaster.Extensions.CommandLineUtils;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace BitbucketMigrationTool.Commands
+{
+    [Command(Name = "test", OptionsComparison = StringComparison.InvariantCultureIgnoreCase)]
+    [VersionOptionFromMember("--version", MemberName = nameof(GetVersion))]
+    internal class TestCommand : CommandBase
+    {
+
+        public TestCommand(ILogger<LargeFileCommand> logger, IOptions<AppSettings> appSettingsOptions, BitbucketClient bitbucketClient, AZDevopsClient azdevopsClient) : base(logger, appSettingsOptions, bitbucketClient, azdevopsClient)
+        {
+        }
+
+        protected override async Task<int> OnExecute(CommandLineApplication app)
+        {
+            // Azure DevOps Projects
+            var projectsAzdo = await aZDevopsClient.GetProjectsAsync();
+
+            await Console.Out.WriteLineAsync($"Number of Azure DevOps Projects: - {projectsAzdo.Count()}");
+
+            // Bitbucket Projects
+            var projectsBB = await bitbucketClient.GetProjectsAsync();
+
+            await Console.Out.WriteLineAsync($"Number of Bitbucket Projects: - {projectsBB.Count()}");
+
+            // Add Other Connections
+
+            return 0;
+        }
+    }
+}
